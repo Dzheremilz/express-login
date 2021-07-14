37eeacf70ad5d346c0bd9e560ac46d9fbecc540d
Dzheremilz <trempejeremie@gmail.com>
2021-07-14 23:40:15 +0200

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+    fetch("http://172.31.103.30:3333/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`Error ${response.statusText}`);
+        }
+        return response.text();
+      })
+      .then((result) => {
+        setLogin(result);
+        setError(false);
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h2 className="text-center">Log in</h2>
+      {login ? (
+        <p className="text-center">{login}</p>
+      ) : (
+        <>
+          <form className="row g-3 mt-3" onSubmit={handleFormSubmit}>
+            <div className="col-auto">
+              <label htmlFor="username" className="visually-hidden">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="username"
+                required
+              />
+            </div>
+            <div className="col-auto">
+              <label htmlFor="password" className="visually-hidden">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary mb-3">
+                Log in
+              </button>
+            </div>
+          </form>
+        </>
+      )}
+      {error && <p className="alert alert-danger">{error}</p>}
+    </section>
   );
 }
 
